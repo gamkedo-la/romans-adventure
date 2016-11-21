a552884f7d3a83101e2bcac625afe4060da78dc5
Chris Markle <christofermarkle@gmail.com>
2016-11-21 14:03:22 -0800

    Accidently overwrote a line in Caspar's last commit. Fixed!

--- js/Dialogue.js
+++ js/Dialogue.js
@@ -1,12 +1,12 @@
 var dialogueStairsPuzzleNotSolved = "Ghost Roman will not allow you to pass."; //"You two can't take your eyes off one another.";
 var dialogueGhostRoman = "Ghost Roman will not allow you to pass."; //"You're always getting in your own way.";
-var dialogueStairsPuzzleSolved = "Ghost Roman is mesmorized by his reflection.";  //"Your vanity knows no bounds.";
+var dialogueStairsPuzzleSolved = "Ghost Roman is mesmerized by his reflection.";  //"Your vanity knows no bounds.";
 
 var dialogueDenPuzzleNotSolved = "Hint: Top left piece of the table is already in the correct spot.";
 var dialogueDenPuzzleSolved = "Dialogue bark for den puzzle solved.";
 
 var dialogueBedroom1PuzzleNotSolved = "Hint: Be careful where you walk!";
-var dialogueBedroom1PuzzleShowPath = "Oh look! That is safe path!";
+var dialogueBedroom1PuzzleShowPath = "Oh look! That is a safe path!";
 var dialogueBedroom1PuzzleSolved = "Dialogue bark for bedroom 1 puzzle solved.";
 
 //var dialogueStudyPuzzleNotSolved = "Sorry, but there are no pictures in these books.";
