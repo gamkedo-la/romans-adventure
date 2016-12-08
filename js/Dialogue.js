1e14f2d542df1663edd6ea2e61f61383fe703632
Chris Markle <christofermarkle@gmail.com>
2016-12-08 10:55:01 -0800

    Added dialogue for Stairs and Study

--- js/Dialogue.js
+++ js/Dialogue.js
@@ -1,6 +1,6 @@
-var dialogueStairsPuzzleNotSolved = "Ghost Roman will not allow you to pass."; //"You two can't take your eyes off one another.";
-var dialogueGhostRoman = "Ghost Roman will not allow you to pass."; //"You're always getting in your own way.";
-var dialogueStairsPuzzleSolved = "Ghost Roman is mesmerized by his reflection.";  //"Your vanity knows no bounds.";
+var dialogueStairsPuzzleNotSolved = "You two can't take your eyes off one another."; 
+var dialogueGhostRoman = "You're always getting in your own way."; 
+var dialogueStairsPuzzleSolved = "Your vanity knows no bounds."; 
 
 var dialogueDiningRoomEntered = "There's a key over there! I should be able to get to it if I push this stuff out of the way.";
 
@@ -10,13 +10,13 @@ var dialogueDenPuzzleSolved = "How lucky can you get?";
 var dialogueAtticPuzzleNotSolved = "Hint: Be careful where you walk!";
 var dialogueAtticPuzzleShowPath = "Oh look! That is a safe path!";
 var dialogueAtticPuzzleSteppedOffPath = "Huh!? What happened? I must've walked on the wrong tiles.";
-var dialogueAtticPuzzleSolved = "*Roman hears something* What's that? oh! Looks like the floor is normal now.";
+var dialogueAtticPuzzleSolved = "*Roman hears something* What's that? Oh! Looks like the floor is normal now.";
 
 var dialogueGardenMiddlePuzzleNotSolved = "Dialogue bark for Garden Middle puzzle.";
 var dialogueGardenMiddlePuzzleSolved = "Dialogue bark for Garden Middle puzzle solved.";
 
-//var dialogueStudyPuzzleNotSolved = "Sorry, but there are no pictures in these books.";
-//var dialogueStudyPuzzleSolved = "Dialogue bark for Study puzzle solved.";
+var dialogueStudyPuzzleNotSolved = "Sorry, but there are no pictures in these books.";
+var dialogueStudyPuzzleSolved = "You read my mind.";
 
 //var dialogueGardenPuzzleNotSolved = "You may catch cold. Maybe it'll become serious...";
 //var dialogueGardenPuzzleSolved = "I am amazed you made it through.";
